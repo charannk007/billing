ff294bd9eed7d121f5bd3dfc5a50ff31a4460ab2
Charan NK <60515249+charannk007@users.noreply.github.com>
2025-06-16 15:04:58 +0530

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,23 +1,20 @@
-
 CREATE DATABASE supermarket_db;
 USE supermarket_db;
 
 CREATE TABLE cashiers (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    is_active BOOLEAN DEFAULT TRUE,
+    phone VARCHAR(15),
+    name VARCHAR(100)
 );
 
-ALTER TABLE cashiers
-ADD COLUMN is_active BOOLEAN DEFAULT TRUE;
-
-
-ALTER TABLE cashiers ADD phone VARCHAR(15);
-ALTER TABLE cashiers ADD name VARCHAR(100);
 
 CREATE TABLE categories (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) UNIQUE NOT NULL
+    name VARCHAR(50) UNIQUE NOT NULL,
+    is_deleted BOOLEAN DEFAULT FALSE
 );
 
 CREATE TABLE stocks (
@@ -27,26 +24,20 @@ CREATE TABLE stocks (
     quantity INT NOT NULL,
     category_id INT,
     barcode VARCHAR(255),
+    is_deleted BOOLEAN DEFAULT FALSE,
     FOREIGN KEY (category_id) REFERENCES categories(id)
 );
 
-
 CREATE TABLE transactions (
-    id VARCHAR(36) PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     cashier_id INT,
     total_amount DECIMAL(10, 2),
     transaction_date DATETIME,
+    total DECIMAL(10, 2),
+    date DATE,
+    time TIME,
     FOREIGN KEY (cashier_id) REFERENCES cashiers(id)
 );
-ALTER TABLE transactions ADD COLUMN total DECIMAL(10,2);
-ALTER TABLE transactions DROP PRIMARY KEY;
-ALTER TABLE transactions MODIFY id INT AUTO_INCREMENT PRIMARY KEY;
-ALTER TABLE transactions
-ADD COLUMN date DATE,
-ADD COLUMN time TIME;
-
-
-
 
 CREATE TABLE transaction_items (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -56,20 +47,3 @@ CREATE TABLE transaction_items (
     FOREIGN KEY (transaction_id) REFERENCES transactions(id),
     FOREIGN KEY (item_id) REFERENCES stocks(id)
 );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
